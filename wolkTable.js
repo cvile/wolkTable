15c61c9ad1411758ab5db7234000bfe8cf3d6a59
cvile <cvilee@gmail.com>
2016-10-26 11:02:59 +0200

    alpha release

--- wolkTable.js
+++ wolkTable.js
@@ -12,6 +12,14 @@
 		.module('wolkTable')
 		.directive('tableResize', tableResizeInject);
 
+	angular
+		.module('wolkTable')
+		.service('defaultConfiguration', defaultConfigurationInject);
+
+	angular
+		.module('wolkTable')
+		.service('errorHandler', errorHandlerInject);
+
 	function tableComponentInject() {
 		var directive = {
 			restrict: 'E',
@@ -37,13 +45,13 @@
 					}
 				});
 
-			}
+			}	
 		};
 
 		return directive;
 
 		/** @ngInject */
-		function tableComponentController() {
+		function tableComponentController($mdDialog) {
 			var vm = this;
 			// list of all column names
 			vm.columns = [];
@@ -52,7 +60,220 @@
 				vm.columns = data;
 			};
 
-		}
+			vm.editRow = function (ev, data) {
+				$mdDialog.show({
+					controller: DialogController,
+					controllerAs : 'vm',
+					template: `
+						<md-dialog aria-label="edit row"  id="editRowDialogContainer" flex-xs="90" flex-sm="80" flex-md="40" flex-lg="60" flex-xl="20">
+							<form name="editRowForm" ng-submit="vm.saveChanges(editRowForm)" novalidate>
+								<md-toolbar id="editDialogHeader">
+									<div class="md-toolbar-tools">
+										<h2>Edit Dialog</h2>
+									</div>
+								</md-toolbar>
+
+		                      	<md-progress-linear class="md-warn md-hue-1" md-mode="indeterminate" ng-if="vm.showLoader"></md-progress-linear>
+
+									<md-dialog-content id="editDialogBody">
+										<div class="md-dialog-content" layout="column">
+
+
+							                <md-input-container class="md-block" ng-repeat="column in vm.columnDetail">
+												<label>{{::column.name || column.id}}</label>
+												<input 
+													ng-model="vm.data[column.id]" 	
+													name="{{::column.id}}"
+													ng-minlength="{{::column.validators.minLength || vm.getDefaultValidation(column.type).minLength}}"
+													ng-maxlength="{{::column.validators.maxLength || vm.getDefaultValidation(column.type).maxLength}}"
+													md-minlength="{{::column.validators.minLength || vm.getDefaultValidation(column.type).minLength}}"
+													md-maxlength="{{::column.validators.maxLength || vm.getDefaultValidation(column.type).maxLength}}"
+													ng-pattern="{{::column.validators.pattern || vm.getDefaultValidation(column.type).pattern}}"
+													type="text">
+
+													<div ng-messages="editRowForm[column.id].$error" layout="column" layout-align="center start" flex ng-messages-multiple>
+														<div flex ng-message="minlength">- {{::column.validators.minLengthMsg || vm.getDefaultValidation(column.type).minLengthErrMsg}}</div>
+														<div flex ng-message="maxlength">- {{::column.validators.maxLengthMsg || vm.getDefaultValidation(column.type).maxLengthErrMsg}}</div>
+														<div flex ng-message="required">- {{::column.validators.requiredMsg || vm.getDefaultValidation(column.type).requiredErrMsg}}</div>
+														<div flex ng-message="pattern">- {{::column.validators.patternMsg || vm.getDefaultValidation(column.type).patternErrMsg}}</div>
+													</div>
+											</md-input-container>
+
+										</div>	
+									</md-dialog-content>
+
+								<md-dialog-actions layout="row" id="editDialogFooter">
+									<span flex></span>
+									<md-button type="submit">
+										save
+									</md-button>
+									<md-button ng-click="vm.cancel()" class="cancelButton">
+										cancel
+									</md-button>
+								</md-dialog-actions>
+							</form>
+						</md-dialog>
+					`,
+					targetEvent: ev,
+					locals : {
+						_data : data,
+						_currentScope : this
+					},
+					clickOutsideToClose:true,
+					escapeToClose : true
+				});
+
+				function DialogController($mdDialog, $mdToast, _data, _currentScope, defaultConfiguration, errorHandler) {
+					var vm = this;
+
+					vm.showLoader = false;
+					vm.data = _data;
+					vm.columnDetail = [];
+					vm.defaultColumnDetail = [];
+					vm.model = {};			
+
+
+					var tableConfig = _currentScope.tableConfig;
+
+					vm.getDefaultValidation = function(_type){
+						return defaultConfiguration.getDefaultValidation(_type);
+					};
+
+
+					// vm.columnDetail EXAMPLE
+					/*
+						vm.columnDetail = [{
+							id : "id",
+							name : "id",
+							validators : {
+								required : true,
+								requiredMsg : "This field is required",
+								minLength : true,
+								minLengthRule : 0,
+								minLengthMsg : "Minimum value for input field",
+								maxLength : true,
+								maxLengthRule : 30,
+								maxLengthMsg : "Maximum input characters ... "
+							}		
+						}] ...
+					*/
+
+					// vm.validators = {};
+
+					var getValidatorRule = function(_columnId, _validatorId){
+						for(var i=0; i < vm.columnDetail.length; i++){
+							if(vm.columnDetail[i].id === _columnId){
+								for(var j=0; j < vm.columnDetail[i].validators.length; j++){
+									return vm.columnDetail[i].validators[j].validatorRule;
+								}
+							}
+						}
+					};
+
+					var getColumnPropertiesById = function(_id){
+						var temp = {};
+						if(typeof tableConfig.columns !== 'undefined'){
+							for(var i=0; i < tableConfig.columns.length; i++){
+								if(tableConfig.columns[i].id === _id){
+									temp['name'] = tableConfig.columns[i].name;
+									temp['type'] = tableConfig.columns[i].type;
+									temp['validators'] = [];
+									temp.validators = tableConfig.columns[i].validators || [];
+								}
+							}
+							return temp;
+						}else{	
+							// show warning messages
+							errorHandler.showWarningMsg('Columns property is not defined, wolkTable will continue to use object keys for mapping.');
+							errorHandler.showWarningMsg('Note - If you dont define column types [number, text, date or money], all columns will be interpreted as strings');
+							errorHandler.showWarningMsg('Note - For fields validation and translations, use columns property to describe data columns');
+							return false;
+						}
+					};
+
+					var addValidationsToData = function (data){
+
+						for(var key in data){
+
+							var columnProperties = getColumnPropertiesById(key);
+
+							if(columnProperties){
+
+								var temp = {}
+
+								temp['id'] = key || null;
+								temp['name'] = columnProperties.name || null;
+								temp['type'] = columnProperties.type || null;
+								temp['validators'] = {};
+									
+								if(Object.keys(columnProperties).length !== 0){
+									for(var j=0; j < columnProperties.validators.length; j++){
+										temp.validators[columnProperties.validators[j].validatorName] = columnProperties.validators[j].validatorRule;	
+									}
+									
+									vm.columnDetail.push(temp);
+								}
+							}else{
+								// return default data
+								var temp = {};
+								
+								temp['id'] = key;
+								temp['name'] = key;
+								temp['type'] = 'text';
+								temp['validators'] = defaultConfiguration.getDefaultValidation('text');
+
+								vm.columnDetail.push(temp);
+							}
+						}
+					};
+
+					var init = function(){
+						addValidationsToData(_data);
+					};
+
+					init();
+
+					vm.saveChanges = function(){
+						// callback from outside
+						vm.showLoader = true;
+						_currentScope.tableConfig.rowEdit.rowEditCallback(vm.data).then(function(){
+							vm.showLoader = false;
+
+							$mdToast.show(
+						      $mdToast.simple()
+						        .textContent('Successfuly updated data!')
+						        .position('top right')
+						        .hideDelay(3000)
+						        .parent('#editRowDialogContainer')
+						        .toastClass('editDialogToastMsg')
+						    );
+						});
+					};
+
+					vm.cancel = function(){
+						$mdDialog.cancel();
+					};
+				} 
+		    };
+
+		    vm.deleteRow = function(ev){
+				var confirm = $mdDialog.confirm()
+					.title('Delete Confirmation')
+					.textContent('Are you sure you want to delete ? All row data will be lost')
+					.ariaLabel('Table row delete dialog')
+					.targetEvent(ev)
+					.ok('Delete')
+					.cancel('Cancel');
+
+				$mdDialog.show(confirm).then(function() {
+					console.log("you successfuly deleted item");
+					// remove from array
+				}, function() {
+					console.log("you canceled delete event");
+				});
+		    };
+
+		};
 	};
 
 	function tableResizeInject(){
@@ -88,7 +309,7 @@
 		                            temp.push(columnSize);
 		                        });
 
-		                        // let controller know that resize is instantiated 
+		                        // let controller know that resize is instantiated je
 		                        scope.$emit('updateColumnSize', temp);
 		                        // cashe new size in local storage for future usage
 		                        // CasheData.setLocalStorage("cashe_columnSize", JSON.stringify(temp));
@@ -102,6 +323,67 @@
 	            
 	        }
 	    };
+	};
+
+	function defaultConfigurationInject(){
+		var that = this;
+
+		this.getDefaultValidation = function(_inputType){
+			
+			switch(_inputType){ // number, text, date, money
+				case 'number':
+					return {
+						minLength : 1,
+						minLengthErrMsg : 'Number should be at least 2 digit long',
+						maxLength : 10,
+						maxLengthErrMsg : 'Number cannot be more then 10 digit long',
+						pattern : '',
+						patternErrMsg : 'This should never happen !xD'
+					};
+					break;
+				case 'text':
+					return {
+						minLength : 5,
+						minLengthErrMsg : 'Text should be at least 5 character long',
+						maxLength : 25,
+						maxLengthErrMsg : 'Text cannot be more then 25 characters',
+						pattern : '',
+						patternErrMsg : 'This should never happen !xD'
+					};
+					break;
+				case 'date':
+					//
+					break;
+				case 'money':
+					//
+					break;
+			}
+		};
+
+		this.getDefaultColumnPropertiesById = function(_id){
+			// temp['name'] = tableConfig.columns[i].name;
+			// temp['type'] = tableConfig.columns[i].type;
+			// temp['validators'] = [];
+			// temp.validators = tableConfig.columns[i].validators || [];
+
+			return {
+				name : "",
+				type : "",
+				validators : []
+			};
+		};
+	};
+
+	function errorHandlerInject(){
+		console.clear()
+		this.showWarningMsg = function(_msg){
+			console.warn(_msg);
+		}
+
+		this.showErrorMsg = function(_msg){
+			console.error(_msg);
+		}
+
 	};
 
 
